7ee8246055b0cccae94830e9b14a65d740905d31
Phelix <phelixmbani@gmail.com>
2024-11-29 16:59:09 +0300

    cash register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, cash_register=None, discount=0):
+    self.cash_register = cash_register
+    self.discount = discount
+    
+  @property
+  def cash_register(self):
+    return self._cash_register
+  
+  @cash_register.setter
+  def cash_register(self, cash_register):
+    self._cash_register = cash_register
+    
+  @property
+  def discount(self):
+    return self._discount
+  
+  @discount.setter
+  def discount(self, discount):
+    self._discount = discount
+    
